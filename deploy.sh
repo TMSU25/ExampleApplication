4944ac677a1f398af1ea253baea040c65de823d4
Kyungsu Kim <roomformilk683@gmail.com>
2025-03-05 21:55:03 +0000

    modify .sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt install -y nodejs npm
 sudo npm install -g pm2
-export PATH=$PATH:/usr/local/bin
 
 # Stop any existing instance
 pm2 stop example_app || true
